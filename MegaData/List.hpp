eae6f4030664057e20982088d6f83583a6589451
bentschet <btsc0323@740s-pl-126.local>
2017-02-23 13:31:18 -0700

    updated List.hpp

--- MegaData/List.hpp
+++ MegaData/List.hpp
@@ -10,6 +10,8 @@
 #define List_h
 
 #include <assert.h>
+#include <iostream>
+#include "../Model/Node.hpp"
 
 using namespace std;
 
@@ -19,10 +21,13 @@ class List
 private:
     int size;
     Node<Type> * front;
+    Node<Type> * end;
 public:
     List<Type>();
     List<Type>(const List<Type> & source);
     ~List<Type>();
+    
+    
     
     void addAtIndex(int index, Type value);
     void add(Type value);
@@ -33,5 +38,95 @@ public:
     Node<Type> * getFront() const;
     
 };
+
+template <class Type>
+void List<Type> :: addFront(Type value)
+{
+    if(size == 0)
+    {
+        Node<Type> * first = new Node<Type>(value);
+        this->front = first;
+        this->end = first;
+    }
+    else
+    {
+        Node<Type> * newFirst = new Node<Type>();
+        newFirst->setNodeData(value);
+        newFirst->setNodePointer(front);
+        //or
+        //Node<Type> * newFirst = new Node<Type>(value, front);
+        this->front = newFirst;
+    }
+    
+    size++;
+}
+
+template <class Type>
+void List<Type> :: addEnd(Type data)
+{
+    Node<Type> * added = new Node<Type>(data);
+    if(size == 0)
+    {
+        this->front = added;
+        this->end = added;
+    }
+    else
+    {
+        end->setNodePointer(added);
+        this->end = added;
+    }
+    
+    size++;
+}
+
+template <class Type>
+void List<Type> :: addAtIndex(int index, Type value)
+{
+    assert (size >= 0 && index <= size);
+    if (index == 0)
+    {
+        addFront(value);
+    }
+    else if(index == size)
+    {
+        addEnd(value);
+    }
+    else
+    {
+        Node<Type> * insertedNode = new Node<Type>(value);
+        Node<Type> * current = front;
+        Node<Type> * previous = nullptr;
+        
+        for(int position = 0; position < index; position++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+        
+        previous->setNodePointer(insertedNode);
+        insertedNode->setNodePointer(current);
+        
+        size++;
+    }
+}
+
+template <class Type>
+Type List<Type> :: remove(int index)
+{
+    assert (size >= 0 && index < size);
+    Type removedData;
+    if
+    {
+        
+    }
+    else if
+    {
+        
+    }
+    else
+    {
+        
+    }
+}
 
 #endif /* List_h */
