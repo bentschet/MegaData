044a8ab06ae187ffd6f3c5a8e8e1738d8243baa2
bentschet <btsc0323@740s-pl-126.local>
2017-03-01 12:52:09 -0700

    updated UML diagram and added DoublyLinkedList.hpp

---
+++ MegaData/DoublyLinkedList.hpp
@@ -0,0 +1,20 @@
+//
+//  DoublyLinkedList.hpp
+//  MegaData
+//
+//  Created by Tschetter, Benjamin on 3/1/17.
+//  Copyright © 2017 Tschetter, Benjamin. All rights reserved.
+//
+
+#ifndef DoublyLinkedList_h
+#define DoublyLinkedList_h
+
+#include "BiDirectionalNode.hpp"
+
+template <class Type>
+class DoublyLinkedList
+{
+    
+};
+
+#endif /* DoublyLinkedList_h */
