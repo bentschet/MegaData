9d6a367a8128139eadf13652b7c668ef8f2adc82
bentschet <btsc0323@740s-pl-126.local>
2017-04-17 11:40:54 -0600

    replaced tree.hpp

--- MegaData/Tree.hpp
+++ MegaData/Tree.hpp
@@ -1,13 +1,32 @@
 //
 //  Tree.hpp
-//  MegaData
+//  MorningMegaProject
 //
-//  Created by Tschetter, Benjamin on 4/13/17.
-//  Copyright © 2017 Tschetter, Benjamin. All rights reserved.
+//  Created by Cody Henrichsen on 3/30/17.
+//  Copyright © 2017 CTEC. All rights reserved.
 //
 
 #ifndef Tree_h
 #define Tree_h
 
+#include <assert.h>
 
+template <class Type>
+class Tree
+{
+public:
+    //Traversals
+    virtual void inOrderTraversal() = 0;
+    virtual void postOrderTraversal() = 0;
+    virtual void preOrderTraversal() = 0;
+    //Informational Methods
+    virtual int getHeight() = 0;
+    virtual int getSize() = 0;
+    virtual bool isComplete() = 0;
+    virtual bool isBalanced() = 0;
+    //Data Methods
+    virtual bool contains(Type value) = 0;
+    virtual void insert(Type itemToInsert) = 0;
+    virtual void remove(Type value) = 0;
+};
 #endif /* Tree_h */
