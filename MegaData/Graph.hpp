836d7052389ecce5535aaa706f04a9ade80de635
bentschet <btsc0323@740s-pl-126.local>
2017-05-09 11:42:46 -0600

    added Graph.hpp

---
+++ MegaData/Graph.hpp
@@ -0,0 +1,43 @@
+//
+//  Graph.hpp
+//  MegaData
+//
+//  Created by Tschetter, Benjamin on 5/9/17.
+//  Copyright © 2017 Tschetter, Benjamin. All rights reserved.
+//
+
+#ifndef Graph_h
+#define Graph_h
+
+#include <set>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+template <class Type>
+class Graph
+{
+private:
+    static const int MAXIMUM = 20;
+    bool adjacencyMatrix[MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> graph, int vertex, bool markedVertices[]);
+public:
+    Graph();
+    ~Graph();
+    
+    void addVertex(const Type& value);
+    void addEdge(int source, int target);
+    void removeEdge(int source, int target);
+    Type& operator [] (int vertex);
+    Type operator [] (int vertex) const;
+    int size() const;
+    bool areConnected(int source, int target)const;
+    std::set<int> neighbors(int vertex) const;
+    void depthFirstTraversal(Graph<Type> graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> graph, int vertex);
+};
+
+
+#endif /* Graph_h */
